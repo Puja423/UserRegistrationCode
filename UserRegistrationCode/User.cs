b270db9fc9db680ee3a29026924f788485d6e327
Puja <pujayadav@gmail.com>
2020-10-17 15:04:48 +0530

    [puja] add .custom exception

--- UserRegistrationCode/User.cs
+++ UserRegistrationCode/User.cs
@@ -1,53 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace UserRegistrationCode
+namespace UserRegistration
 {
-
     public class User
     {
-        public string firstName { get; set; }
-        private string _regexFirstName = "^[A-Z][a-z]{2,}$";
-        public string lastName { get; set; }
-        private string _regexLastName = "^[A-Z][a-z]{2,}$";
+        string _firstName;
+        string _lastName;
+        string _eMail;
+        string _phoneNumber;
+        string _password;
 
-        public string email { get; set; }
-        private string _regexEmail = "^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        string _regex = "";
 
-        public string password { get; set; }
-        private string _regexPassword = "^(?=.*[A-Z])(?=.*[0-9])[\\S]{8,}$";
-
-        public string mobileNumber { get; set; }
-        private string _regexMobileNumber = "^[0-9]{2}[ ][1-9][0-9]{9}$";
-
-
-        public bool ValidateFirstName(string firstName)
+        public User()
         {
-            return Regex.IsMatch(firstName, _regexFirstName);
+            this._firstName = "";
+            this._lastName = "";
+            this._eMail = "";
+            this._phoneNumber = "";
+            this._password = "";
         }
 
-        public bool ValidateLastName(string lastName)
+        public string FirstName { get => _firstName; }
+        public string LastName { get => _lastName; }
+        public string EMail { get => _eMail; }
+        public string PhoneNumber { get => _phoneNumber; }
+
+        private bool Validate(string strToValidate, string regex)
         {
-            return Regex.IsMatch(lastName, _regexLastName);
+            Regex rgxObj = new Regex(_regex);
+            return rgxObj.IsMatch(strToValidate);
         }
 
-        public bool ValidateEmail(string email)
+        public bool VerifyFirstName(string fName)
         {
-            return Regex.IsMatch(email, _regexEmail);
+            _regex = "^[A-Z][a-z A-Z]{2,}$";
+            if (Validate(fName, _regex))
+                return true;
+            else
+            {
+                string rule = "First name needs min 3 characters and first letter in upper case";
+                throw new ValidationException(ValidationException.InvalidationType.INVALID_FIRST_NAME, rule);
+            }
         }
 
-        public bool ValidatePassword(string password)
+        public bool VerifyLastName(string lName)
         {
-            return Regex.IsMatch(password, _regexPassword);
-        }
-        public bool ValidateMobileNumber(string mobileNumber)
-        {
-            return Regex.IsMatch(mobileNumber, _regexMobileNumber);
+            _regex = "^[A-Z][a-z A-Z]{2,}$";
+            if (Validate(lName, _regex))
+                return true;
+            else
+            {
+                string rule = "Last name needs min 3 characters and first letter in upper case";
+                throw new ValidationException(ValidationException.InvalidationType.INVALID_LAST_NAME, rule);
+            }
         }
 
+        public bool VerifyEmail(string eMail)
+        {
+            _regex = "^[a-z0-9A-Z]+([-.+_][a-z0-9+-]+)*@[a-z0-9A-Z]+[.][a-z]{2,3}([.][a-z]{2,})?$";
+            if (Validate(eMail, _regex))
+                return true;
+            else
+            {
+                string rule = "Email id should be in \" abc.xyz@bl.co.in \" format (.xyz & .in parts optional)";
+                throw new ValidationException(ValidationException.InvalidationType.INVALID_EMAIL, rule);
+            }
+        }
+
+        public bool VerifyPhNumber(string phNum)
+        {
+            _regex = "^[0-9]{2}[ ][1-9][0-9]{9}$";
+            if (Validate(phNum, _regex))
+                return true;
+            else
+            {
+                string rule = "Phone number should have 2 digit country code followed by space separated 10 digit number";
+                throw new ValidationException(ValidationException.InvalidationType.INVALID_PHONE_NUMBER, rule);
+            }
+        }
+
+        public bool VerifyPassword(string pass)
+        {
+            _regex = "((?=^.*[0-9].*$)(?=^.*[A-Z].*$)(?=^[a-zA-Z0-9]*[!@#$%&*+_]{1}[a-zA-Z0-9]*$).{8,})";
+            if (Validate(pass, _regex))
+                return true;
+            else
+            {
+                string rule = "Password needs to have min 8 characters, at least 1 upper case character, at least 1 numeric value, exactly 1 special character";
+                throw new ValidationException(ValidationException.InvalidationType.INVALID_PASSWORD, rule);
+            }
+        }
+
+        public void VerifiedEmailList(List<string> emailList)
+        {
+            foreach (string email in emailList)
+            {
+                if (VerifyEmail(email))
+                    Console.WriteLine(email + "- VALID");
+                else
+                    Console.WriteLine(email + "- INVALID");
+            }
+        }
     }
 }
-
 
--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -1,29 +1,94 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Text;
+using UserRegistration;
 using UserRegistrationCode;
-namespace UserRegistrationTests
+namespace UserValidationTesting
 {
     [TestClass]
-    public class UserTest
+    public class ValidationTesting
     {
         [TestMethod]
-        public void VerifyFirstName_StartsWith_CapitalOrNot()
+        [DataRow("Jyoti")]
+        [DataRow("JYOTI")]
+        [DataRow("Jyoti Ranjan")]
+        public void Test_ValidFirstNames(string fName)
         {
+            //Arrange
             User user = new User();
-            var firstName = "Priya";
-            var result = user.ValidateFirstName(firstName); ;
-            Assert.IsTrue(result);
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyFirstName(fName);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void VerifyLastName_StartsWith_CapitalOrNot()
+        [DataRow("Jr")]
+        [DataRow("jyoti")]
+        [DataRow("Jy@ti")]
+        public void Test_InvalidFirstNames(string fName)
         {
+            //Arrange
             User user = new User();
-            var lastName = "Biriya";
-            var result = user.ValidateLastName(lastName); ;
-            Assert.IsTrue(result);
+            string expected = "First name needs min 3 characters and first letter in upper case";
+            string actual;
+            //Act
+            try
+            {
+                actual = user.VerifyFirstName(fName).ToString();
+            }
+            catch (ValidationException e)
+            {
+                actual = e.Message;
+            }
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        [DataRow("Mishra")]
+        [DataRow("MISHRA")]
+        public void Test_ValidLastNames(string lName)
+        {
+            //Arrange
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyLastName(lName);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("Mi")]
+        [DataRow("mishra")]
+        [DataRow("Mis#ra")]
+        public void Test_InvalidLastNames(string lName)
+        {
+            //Arrange
+            User user = new User();
+            string expected = "Last name needs min 3 characters and first letter in upper case";
+            string actual;
+            //Act
+            try
+            {
+                actual = user.VerifyLastName(lName).ToString();
+            }
+            catch (ValidationException e)
+            {
+                actual = e.Message;
+            }
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
 
         [TestMethod]
         [DataRow("jrm@gmail.com")]
@@ -31,31 +96,91 @@ namespace UserRegistrationTests
         [DataRow("jrm@gmail.co.in")]
         [DataRow("jrm100@gmail.com")]
         [DataRow("jrm-nit@1mg.co.in")]
-        public void TestEmailValidation_ValidEmails(string email)
+        public void Test_ValidEmails(string email)
         {
             //Arrange
             User user = new User();
             bool expected = true;
 
             //Act
-            bool actual = user.ValidateEmail(email);
+            bool actual = user.VerifyEmail(email);
 
             //Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
+        [DataRow("jrm.@gmail.com")]
+        [DataRow("jrm@cg@gmail.co.in")]
+        [DataRow("jrm@gmail.co.in.uk")]
+        [DataRow("jrm100@.gmail.com")]
+        [DataRow("jrm..nit@1mg.com")]
+        [DataRow("jrm.nit.@gmail.com")]
+        [DataRow("jrm..nit@1mg.23")]
+        public void Test_InvalidEmails(string email)
+        {
+            //Arrange
+            User user = new User();
+            string expected = "Email id should be in \" abc.xyz@bl.co.in \" format (.xyz & .in parts optional)";
+            string actual;
+            //Act
+            try
+            {
+                actual = user.VerifyEmail(email).ToString();
+            }
+            catch (ValidationException e)
+            {
+                actual = e.Message;
+            }
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
         [DataRow("91 6231230127")]
-        public void TestPhNumberValidation_ValidNumbers(string phNum)
+        public void Test_ValidNumbers(string phNum)
         {
             //Arrange
             User user = new User();
             bool expected = true;
+
             //Act
-            bool actual = user.ValidateMobileNumber(phNum);
+            bool actual = user.VerifyPhNumber(phNum);
+
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        [DataRow("1234567890")]
+        [DataRow("981236540987")]
+        [DataRow("97 0123456789")]
+        [DataRow("97 123456")]
+        [DataRow("9 1234567890")]
+        [DataRow("97 8123456789012")]
+        [DataRow("97 123 456 6789")]
+        public void Test_InvalidNumbers(string phNum)
+        {
+            //Arrange
+            User user = new User();
+            string expected = "Phone number should have 2 digit country code followed by space separated 10 digit number";
+            string actual;
+            //Act
+            try
+            {
+                actual = user.VerifyPhNumber(phNum).ToString();
+            }
+            catch (ValidationException e)
+            {
+                actual = e.Message;
+            }
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
         [TestMethod]
         [DataRow("Had2Te#t")]
         [DataRow("had2T3stP@ss")]
@@ -65,10 +190,39 @@ namespace UserRegistrationTests
             //Arrange
             User user = new User();
             bool expected = true;
+
             //Act
-            bool actual = user.ValidatePassword(pass);
+            bool actual = user.VerifyPassword(pass);
+
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        [DataRow("Hd2T#t")] //less than 8 char
+        [DataRow("had2testPass")] //no special char
+        [DataRow("TestP@ss")] //no number
+        [DataRow("t3stp@ss")] //no upper case
+        [DataRow("T#stP@ss5")] //more than 1 special case
+        public void Test_InvalidPasswords(string pass)
+        {
+            //Arrange
+            User user = new User();
+            string expected = "Password needs to have min 8 characters, at least 1 upper case character, at least 1 numeric value, exactly 1 special character";
+            string actual;
+            //Act
+            try
+            {
+                actual = user.VerifyPassword(pass).ToString();
+            }
+            catch (ValidationException e)
+            {
+                actual = e.Message;
+            }
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
