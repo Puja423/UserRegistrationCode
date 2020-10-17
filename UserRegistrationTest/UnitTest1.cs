96d2f3cee9f7784552c4dcb02ae888d83ba58e80
Puja <pujayadav@gmail.com>
2020-10-17 14:55:26 +0530

    [puja] add . Test For validation

--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -1,74 +1,162 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Text;
-using UserRegistrationCode;
-namespace UserRegistrationTests
+using UserRegistration;
+
+namespace UserValidationTesting
 {
     [TestClass]
-    public class UserTest
+    public class ValidationTesting
     {
         [TestMethod]
-        public void VerifyFirstName_StartsWith_CapitalOrNot()
-        {
-            User user = new User();
-            var firstName = "Priya";
-            var result = user.ValidateFirstName(firstName); ;
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void VerifyLastName_StartsWith_CapitalOrNot()
-        {
-            User user = new User();
-            var lastName = "Biriya";
-            var result = user.ValidateLastName(lastName); ;
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        [DataRow("jrm@gmail.com")]
-        [DataRow("jrm.cg@gmail.co.in")]
-        [DataRow("jrm@gmail.co.in")]
-        [DataRow("jrm100@gmail.com")]
-        [DataRow("jrm-nit@1mg.co.in")]
-        public void TestEmailValidation_ValidEmails(string email)
+        public void Test_ValidFirstName()
         {
             //Arrange
+            string fName = "Jyoti";
             User user = new User();
             bool expected = true;
 
             //Act
-            bool actual = user.ValidateEmail(email);
+            bool actual = user.VerifyFirstName(fName);
 
             //Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        [DataRow("91 6231230127")]
-        public void TestPhNumberValidation_ValidNumbers(string phNum)
+        public void Test_InvalidFirstName()
         {
             //Arrange
+            string fName = "jyoti";
             User user = new User();
             bool expected = true;
+
             //Act
-            bool actual = user.ValidateMobileNumber(phNum);
+            bool actual = user.VerifyFirstName(fName);
+
             //Assert
-            Assert.AreEqual(expected, actual);
+            Assert.AreNotEqual(expected, actual);
         }
+
         [TestMethod]
-        [DataRow("Had2Te#t")]
-        [DataRow("had2T3stP@ss")]
-        [DataRow("T3STP@SS")]
-        public void Test_ValidPasswords(string pass)
+        public void Test_ValidLastName()
         {
             //Arrange
+            string lName = "Mishra";
             User user = new User();
             bool expected = true;
+
             //Act
-            bool actual = user.ValidatePassword(pass);
+            bool actual = user.VerifyLastName(lName);
+
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_InvalidLastName()
+        {
+            //Arrange
+            string lName = "mishra";
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyLastName(lName);
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_ValidEmail()
+        {
+            //Arrange
+            string email = "jrm@gmail.co.in";
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyEmail(email);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_InvalidEmail()
+        {
+            //Arrange
+            string email = "jrm..100@gmail.co.in";
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyEmail(email);
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_ValidNumber()
+        {
+            //Arrange
+            string phNum = "91 6231230127";
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyPhNumber(phNum);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_InvalidNumber()
+        {
+            //Arrange
+            string phNum = "97 8123456789012";
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyPhNumber(phNum);
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_ValidPassword()
+        {
+            //Arrange
+            string pass = "Had2Te#t";
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyPassword(pass);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_InvalidPassword()
+        {
+            //Arrange
+            string pass = "Hd2T#t";
+            User user = new User();
+            bool expected = true;
+
+            //Act
+            bool actual = user.VerifyPassword(pass);
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+
     }
 }
